a6af4f6b9e6560e01813db238c329efb386c196e
Ryan Ray <ryanray25@hotmail.com>
2024-11-25 08:55:34 -0700

    polished setup.py a bit more

--- README.md
+++ README.md
@@ -6,13 +6,20 @@
 - waybar
 - hyprland
 - hyprpaper
+- rofi-wayland
 
 Heres a quick command to install dependencies on Arch:
 ```bash
-sudo pacman -S waybar hyprland hyprpaper
+sudo pacman -S python3 waybar hyprland hyprpaper rofi-wayland
 ```
 
 ### Installation
 1. Install the required Programs
 2. Clone this repository
 3. Run the install script using Python 3 (Or manually copy files in the config folder)
+
+### Setup File
+I created a setup file that dramaticlly simplifies the setup process (for new linux installs)
+- Make sure to install the required programs first
+- Run the setup file using Python (`python3 setup.py`)
+- Additional options can be found by running `python3 setup.py --help`
--- setup.py
+++ setup.py
@@ -1,19 +1,24 @@
 import argparse
+import shutil
 import os
 
-AUTOSETUP_VER = '1.0.0'
+AUTOSETUP_VER = '1.1.0'
 
 args = argparse.ArgumentParser(description='Automatic Config File Setup')
 args.add_argument('-y', '--yes', action='store_true', help='Automatically answer yes to all prompts')
 args.add_argument('-u', '--update', action='store_true', help='Update the script & config files')
 args.add_argument('-v', '--version', action='version', version=AUTOSETUP_VER)
+args.add_argument('--hyprland', action='store_true', help='Automatically Setup ONLY Hyprland config files')
+args.add_argument('--waybar', action='store_true', help='Automatically Setup ONLY Waybar config files')
+args.add_argument('--wallpaper', action='store_true', help='Automatically Setup ONLY Wallpaper config files')
+args.add_argument('--rofi', action='store_true', help='Automatically Setup ONLY Rofi config files')
 
 def HyprlandSetup(username, file_location_dir):
     print('\033[96mSymlinking Hyprland config files...\033[0m')
     try:
         if os.path.exists(f'/home/{username}/.config/hypr'):
             raise FileExistsError
-        os.symlink(f'{file_location_dir}/hypr', f'/home/{username}/.config/hypr')
+        os.symlink(f'{file_location_dir}/hypr', f'/home/{username}/.config/hypr', target_is_directory=True)
 
     except FileExistsError:
         print('\033[93mHyprland config files already exist. Skipping...\033[0m')
@@ -26,7 +31,7 @@ def WaybarSetup(username, file_location_dir):
     try:
         if os.path.exists(f'/home/{username}/.config/waybar'):
             raise FileExistsError
-        os.symlink(f'{file_location_dir}/waybar', f'/home/{username}/.config/waybar')
+        os.symlink(f'{file_location_dir}/waybar', f'/home/{username}/.config/waybar', target_is_directory=True)
 
     except FileExistsError:
         print('\033[93mWaybar config files already exist. Skipping...\033[0m')
@@ -35,7 +40,7 @@ def WaybarSetup(username, file_location_dir):
         print(f'\033[91mError: {e}\033[0m')
 
 def WallpaperSetup(username, file_location_dir):
-    print('\033[96mCopying Wallpaper config files...\033[0m')
+    print('\033[96mCopying Wallpapers...\033[0m')
     try:
         if not (os.path.exists(f'/home/{username}/Wallpapers')):
             os.mkdir(f'/home/{username}/Wallpapers')
@@ -46,40 +51,92 @@ def WallpaperSetup(username, file_location_dir):
     except Exception as e:
         print(f'\033[91mError: {e}\033[0m')
 
+def RofiSetup(username, file_location_dir):
+    def LinkSetupFile(file_location_dir):
+        try:
+            if os.path.exists(f'{file_location_dir}/configs/rofi/'):
+                raise FileExistsError
+
+            else:
+                os.symlink(f'{file_location_dir}/configs/rofi/', f'/home/{username}/.config/rofi', target_is_directory=True)
+
+        except FileExistsError:
+            print('\033[93mRofi config files already exist. Skipping...\033[0m')
+
+        except Exception as e:
+            print(f'\033[91mError: {e}\033[0m')
+
+
+    print('\033[96mFetching rofi theme files...\033[0m')
+    try:
+        file_link = 'https://raw.githubusercontent.com/newmanls/rofi-themes-collection/refs/heads/master/themes/spotlight-dark.rasi'
+        config_dir = f'/home/{username}/.local/share/rofi/themes'
+
+        if not os.path.exists(config_dir):
+            os.mkdir(config_dir)
+
+        if os.path.exists(f'{config_dir}/spotlight.rasi'):
+            raise FileExistsError
+
+        if shutil.which('wget'):
+            # Download with wget if installed
+            os.system(f'wget {file_link} -O {config_dir}/spotlight.rasi')
+            
+        elif shutil.which('curl'):
+            # Download with curl if installed
+            os.system(f'curl {file_link} -o {config_dir}/spotlight.rasi')
+
+        else:
+            raise shutil.ExecError
+
+        LinkSetupFile(file_location_dir)
+
+    except shutil.ExecError:
+        print('\033[91mError: wget or curl not found. Cannot download rofi config files.\033[0m')
+
+    except FileExistsError:
+        print('\033[93mRofi config files already exist. Copying setup file...\033[0m')
+        LinkSetupFile(file_location_dir)
+
 def main(autoyes: bool = False):
     AutoYes = autoyes
     username = os.getlogin()
     file_location_dir = os.path.abspath(__file__).replace('setup.py', '')
+
+    setups = {
+        'Hyprland': [
+            '\nWould you like to symlink the Hyprland config files? (y/n)',
+            HyprlandSetup,
+            'Skipping Hyprland config file setup...'
+        ],
+        'Waybar': [
+            '\nWould you like to symlink the Waybar config files? (y/n)',
+            WaybarSetup,
+            'Skipping Waybar config file setup...'
+        ],
+        'Wallpaper': [
+            '\nWould you like to copy the Wallpapers (y/n)',
+            WallpaperSetup,
+            'Skipping Wallpaper config file setup...'
+        ],
+        'Rofi': [
+            '\nWould you like to setup the Rofi config files? (y/n)',
+            RofiSetup,
+            'Skipping Rofi config file setup...'
+        ]
+    }
     
-    # Hyprland Setup
-    if not AutoYes:
-        print('\nWould you like to symlink the Hyprland config files? (y/n)')
-        if input().lower() == 'y':
-            HyprlandSetup(username, file_location_dir)
+    # Did this to condense the code a little bit
+    for setup_option in setups:
+        if not AutoYes:
+            print(setups[setup_option][0])
+            if input().lower() == 'y':
+                setups[setup_option][1](username, file_location_dir)
+            else:
+                print(setups[setup_option][2])
         else:
-            print('Skipping Hyprland config file setup...')
-    else:
-        HyprlandSetup(username, file_location_dir)
-
-    # Waybar Setup
-    if not AutoYes:
-        print('\nWould you like to symlink the Waybar config files? (y/n)')
-        if input().lower() == 'y':
-            WaybarSetup(username, file_location_dir)
-        else:
-            print('Skipping Waybar config file setup...')
-    else:
-        WaybarSetup(username, file_location_dir)
-        
-
-    # Wallpaper Setup
-    if not AutoYes:
-        print('\nWould you like to copy the Wallpaper config files? (y/n)')
-        if input().lower() == 'y':
-            WallpaperSetup(username, file_location_dir)
-        else:
-            print('Skipping Wallpaper config file setup...')
-
+            setups[setup_option][1](username, file_location_dir)
+   
     print('Setup complete!')
         
 
@@ -89,6 +146,14 @@ if __name__ == '__main__':
         os.system('git pull')
         print("\033[91mPlease relaunch the script\033[0m")
         exit(0)
+    elif args.hyprland:
+        HyprlandSetup(os.getlogin(), os.path.abspath(__file__).replace('setup.py', ''))
+    elif args.waybar:
+        WaybarSetup(os.getlogin(), os.path.abspath(__file__).replace('setup.py', ''))
+    elif args.wallpaper:
+        WallpaperSetup(os.getlogin(), os.path.abspath(__file__).replace('setup.py', ''))
+    elif args.rofi:
+        RofiSetup(os.getlogin(), os.path.abspath(__file__).replace('setup.py', ''))
     elif args.yes:
         main(autoyes=True)
     else:
